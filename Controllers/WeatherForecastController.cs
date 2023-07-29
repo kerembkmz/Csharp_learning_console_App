25ecf53b2340ec06bf2e99ab8f2dc58d6870f470
kerembkmz <101328913+kerembkmz@users.noreply.github.com>
2023-07-29 19:22:29 +0300

    Implemented all of the necessary files, cross checked them from different sources but cannot fix the status code:500, internal service error.

---
+++ Controllers/WeatherForecastController.cs
@@ -0,0 +1,50 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using ForecastApp.ForecastAppModels;
+using ForecastApp.OpenWeatherMapModels;
+using ForecastApp.Repositories;
+using Microsoft.AspNetCore.Http;
+using Microsoft.AspNetCore.Mvc;
+
+namespace ForecastApp.Controllers
+{
+    [ApiController]
+    [Route("api/[controller]")]
+    public class ForecastAppController : ControllerBase
+    {
+        private readonly IForecastRepository _forecastRepository;
+
+        // Dependency Injection
+        public ForecastAppController(IForecastRepository forecastAppRepo)
+        {
+            _forecastRepository = forecastAppRepo;
+        }
+
+        
+
+
+        // GET: api/ForecastApp/City
+        [HttpGet("{city}")]
+        public IActionResult City(string city)
+        {
+            // Consume the OpenWeatherAPI in order to bring Forecast data in our page.
+            WeatherResponse weatherResponse = _forecastRepository.GetForecast(city);
+            if (weatherResponse != null)
+            {
+                var viewModel = new City
+                {
+                    Name = weatherResponse.Name,
+                    Humidity = weatherResponse.Main.Humidity,
+                    Pressure = weatherResponse.Main.Pressure,
+                    Temp = weatherResponse.Main.Temp,
+                    Weather = weatherResponse.Weather[0].Main,
+                    Wind = weatherResponse.Wind.Speed
+                };
+                return Ok(viewModel);
+            }
+            return NotFound();
+        }
+    }
+}
--- Program.cs
+++ Program.cs
@@ -1,176 +1,26 @@
-﻿using System;
-using System.Diagnostics;
-using Newtonsoft.Json; //Newtonsoft.Json From NuGet Packages.
+﻿var builder = WebApplication.CreateBuilder(args);
 
-using System.Net.Http; //To Use Http Client
-using System.Threading.Tasks;
+// Add services to the container.
 
-//Request Library.
-using System.Net;
-using System.IO;
-using Csharp_weather_app// Note: actual namespace depends on the project name.
-;
+builder.Services.AddControllers();
+// Learn more about configuring Swagger/OpenAPI at https://aka.ms/aspnetcore/swashbuckle
+builder.Services.AddEndpointsApiExplorer();
+builder.Services.AddSwaggerGen();
 
+var app = builder.Build();
 
-//This way, HttpClient is instantiated once per application, rather than per-use
-//So it is more efficient on paper.
-HttpClient client = new HttpClient();
-
-async Task<string> getWeatherAPIUsingHTTPClient(string location)
+// Configure the HTTP request pipeline.
+if (app.Environment.IsDevelopment())
 {
-    using var client = new HttpClient();
-    //var url = "http://api.openweathermap.org/data/2.5/weather?q=" + location + "&appid=" + All_Keys.GetWeatherAPIKey();
-    //var url = "https://c49f93e3-f002-4bff-9e67-175b36c66809.mock.pstmn.io" + "?q" + location + "&appid=" + All_Keys.GetWeatherAPIKey();
-    var url = "https://42a4ae73-9598-47c4-9e27-ac0791695b0b.mock.pstmn.io" + "?q" + location + "&appid=" + All_Keys.GetWeatherAPIKey();
-    var response = await client.GetAsync(url);
-    return await response.Content.ReadAsStringAsync();
+    app.UseSwagger();
+    app.UseSwaggerUI();
 }
 
-//Console.WriteLine(API_Keys.GetWeatherAPIKey());  
-//Console.WriteLine(API_Keys.GetPositionStackAPIKey());
-//Testing to see whether the correct returns are satisfied.
+app.UseHttpsRedirection();
 
+app.UseAuthorization();
 
-string CityInput = "";
-string choice = "";
+app.MapControllers();
 
-while (CityInput != "q" || choice != "q")
-{
-
-    Console.WriteLine("To quit, press q");
-    Console.WriteLine("To sign in, ype Sign In");
-    Console.WriteLine("To sign up, type Sign Up");
-
-
-    choice = Console.ReadLine();
-    DB_Conn connection = new DB_Conn();
-
-    if (choice == "Sign In") {
-        Console.Write("Username: ");
-        string username = Console.ReadLine();
-        Console.Write("Password: ");
-        string password = Console.ReadLine();
-        
-
-        if (connection.CheckValidationUser(username, password))
-        {
-
-
-            while (CityInput != "q") {
-                Console.Write("City: ");
-                CityInput = Console.ReadLine();
-
-
-                //Stopwatch stopwatch1 = new Stopwatch();
-                //Stopwatch stopwatch2 = new Stopwatch();
-
-                //stopwatch1.Start();
-                //string jsonObj2 = await getWeatherAPIUsingHTTPClient(CityInput);
-                //Console.WriteLine(jsonObj2);
-                //stopwatch1.Stop();
-
-                //Console.WriteLine("Time using HTTP: " + stopwatch1.ElapsedMilliseconds);
-
-
-                //stopwatch2.Start();
-                string jsonObj = getWeatherAPI(CityInput);
-                //Console.WriteLine(jsonObj);
-                //stopwatch2.Stop();
-
-                //Console.WriteLine("Time using webRequest: " + stopwatch2.ElapsedMilliseconds);
-
-
-                var weatherData = JsonConvert.DeserializeObject<dynamic>(jsonObj); //Using Newtonsoft NuGet package to deserialize the json object.
-                                                                                   //Making deserializing to a dynamic type so that it is easier to read & grasp the code.
-                                                                                   //Also solves the non-nullable run.
-
-                Console.WriteLine("Server: " + weatherData.weather[0].server);
-                Console.WriteLine("Description: " + weatherData.weather[0].description);
-                Console.WriteLine("Temperature: " + (weatherData.main.temp - 273.15));
-                Console.WriteLine("Location: " + weatherData.name);
-                Console.WriteLine("Humidity: " + weatherData.main.humidity);
-                Console.WriteLine("Wind Speed: " + weatherData.wind.speed);
-            }
-
-        }
-        else {
-            Console.WriteLine("Please try again, wrong credentials.");
-        }
-        
-    }
-    else if (choice == "Sign Up") // Add the "Sign Up" option and implement sign-up functionality
-    {
-        Console.Write("New Username: ");
-        string newUsername = Console.ReadLine();
-        Console.Write("New Password: ");
-        string newPassword = Console.ReadLine();
-
-        connection.RegisterUser(newUsername, newPassword);
-
-        Console.WriteLine("User registered successfully!");
-        Console.WriteLine("Welcome to the weather app!");
-
-        while (CityInput != "q")
-        {
-            Console.Write("City: ");
-            CityInput = Console.ReadLine();
-
-
-            //Stopwatch stopwatch1 = new Stopwatch();
-            //Stopwatch stopwatch2 = new Stopwatch();
-
-            //stopwatch1.Start();
-            //string jsonObj2 = await getWeatherAPIUsingHTTPClient(CityInput);
-            //Console.WriteLine(jsonObj2);
-            //stopwatch1.Stop();
-
-            //Console.WriteLine("Time using HTTP: " + stopwatch1.ElapsedMilliseconds);
-
-
-            //stopwatch2.Start();
-            string jsonObj = getWeatherAPI(CityInput);
-            //Console.WriteLine(jsonObj);
-            //stopwatch2.Stop();
-
-            //Console.WriteLine("Time using webRequest: " + stopwatch2.ElapsedMilliseconds);
-
-
-            var weatherData = JsonConvert.DeserializeObject<dynamic>(jsonObj); //Using Newtonsoft NuGet package to deserialize the json object.
-                                                                               //Making deserializing to a dynamic type so that it is easier to read & grasp the code.
-                                                                               //Also solves the non-nullable run.
-
-            Console.WriteLine("Description: " + weatherData.weather[0].description);
-            Console.WriteLine("Temperature: " + (weatherData.main.temp - 273.15));
-            Console.WriteLine("Location: " + weatherData.name);
-            Console.WriteLine("Humidity: " + weatherData.main.humidity);
-            Console.WriteLine("Wind Speed: " + weatherData.wind.speed);
-        }
-    }
-
-
-
-
-
-
-
-}
-
-
-
-string getWeatherAPI(string location)
-{
-    var url = "https://54c42091-68dc-4c8e-8c04-057eb70dbb5a.mock.pstmn.io" + "?q=" + location + "&appid=" + All_Keys.GetWeatherAPIKey(); ;
-    var request = WebRequest.Create(url);
-    request.Method = "GET";
-
-    using var webResponse = request.GetResponse();
-    using var webStream = webResponse.GetResponseStream();
-
-    using var reader = new StreamReader(webStream);
-    var data = reader.ReadToEnd();
-
-
-    return data;
-
-}
+app.Run();
 
